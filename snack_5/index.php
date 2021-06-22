212a84e42f9987860c56a48a83fbd365dc5984f9
andreagavardi <andreagavardi89@gmail.com>
2021-06-22 18:09:59 +0200

    inizializzato snack 5

---
+++ snack_5/index.php
@@ -0,0 +1,11 @@
+<!-- Snack 5
+Prendere un paragrafo abbastanza lungo, contenente diverse frasi.
+ Prendere il paragrafo e suddividerlo in tanti paragrafi. 
+ Ogni punto un nuovo paragrafo. -->
+
+<?php
+$paragrafo = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque, aliquid asperiores, sunt error quas labore eius cum odit dolor aut quos consequatur vero incidunt est molestias ullam nemo nobis maiores. quam nam quod doloribus iure nisi reiciendis! Accusantium iure quo velit distinctio, adipisci voluptate perferendis odit ullam quam vitae non.";
+
+
+
+?>
